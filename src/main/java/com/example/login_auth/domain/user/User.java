48c5a2517ac751918794d75c18a347eddebb573a
kauanmachado <kauan.smachado0@gmail.com>
2025-01-16 07:33:16 -0300

    v1

---
+++ src/main/java/com/example/login_auth/domain/user/User.java
@@ -0,0 +1,20 @@
+package com.example.login_auth.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/example/login_auth/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.login_auth.repositories;
+
+import com.example.login_auth.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+
+}
